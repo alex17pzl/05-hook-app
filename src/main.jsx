8d583b829cb5045b65617275173b94940e94efe1
Alejandro <alex_avila_solera@hotmail.es>
2023-09-03 19:45:24 +0200

    intro-reducer.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './08-useReducer/intro-reducer';
 import { Padre } from './07-tarea-memo/Padre';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Padre />
+//   </React.StrictMode>
+// );

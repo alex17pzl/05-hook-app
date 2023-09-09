2244ba50a6d924e3e065c4e8c838a5d76903f6ec
Alejandro <alex_avila_solera@hotmail.es>
2023-09-09 19:32:53 +0200

    Installed react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainApp />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TodoApp />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

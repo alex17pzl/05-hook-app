612743e5ee1c384bcd00665874e86eae516dba21
Alejandro <alex_avila_solera@hotmail.es>
2023-09-16 16:54:01 +0200

    todo ok

--- tests/09-useContext/MainApp.test.jsx
+++ tests/09-useContext/MainApp.test.jsx
@@ -1,36 +1,32 @@
 import { render, screen } from '@testing-library/react';
-import { Navigate, RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { MainApp } from '../../src/09-useContext/MainApp';
-import { HomePage } from '../../src/09-useContext/HomePage';
-import { AboutPage } from '../../src/09-useContext/AboutPage';
-import { LoginPage } from '../../src/09-useContext/LoginPage';
-// import { routerConfig } from '../../src/main';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { routerConfig } from '../../src/main';
 
 describe('Pruebas en <MainApp />', () => {
-  const routerConfig = [
-    {
-      path: '/',
-      element: <MainApp />,
-      children: [
-        {
-          path: '/',
-          element: <HomePage />,
-        },
-        {
-          path: 'about',
-          element: <AboutPage />,
-        },
-        {
-          path: 'login',
-          element: <LoginPage />,
-        },
-        {
-          path: '*',
-          element: <Navigate to="/" replace />,
-        },
-      ],
-    },
-  ];
+  // const routerConfig = [
+  //   {
+  //     path: '/',
+  //     element: <MainApp />,
+  //     children: [
+  //       {
+  //         path: '/',
+  //         element: <HomePage />,
+  //       },
+  //       {
+  //         path: 'about',
+  //         element: <AboutPage />,
+  //       },
+  //       {
+  //         path: 'login',
+  //         element: <LoginPage />,
+  //       },
+  //       {
+  //         path: '*',
+  //         element: <Navigate to="/" replace />,
+  //       },
+  //     ],
+  //   },
+  // ];
 
   test('debe de mostrar el HomePage', () => {
     const router = createMemoryRouter(routerConfig, {
